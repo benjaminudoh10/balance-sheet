68b301d5a9a8d53ec315c21818861b4a6d166527
Benjamin Udoh <benjaminudoh10@gmail.com>
2026-04-17 13:17:53 +0100

    feat: refresh db version to start from v1
    Signed-off-by: Benjamin Udoh <benjaminudoh10@gmail.com>

--- lib/constants/db.dart
+++ lib/constants/db.dart
@@ -7,8 +7,6 @@ class DBConstants {
   static const INVESTMENT_HOLDING = "investment_holdings";
   static const INVESTMENT_LOT = "investment_lot_entries";
   static const INVESTMENT_PRICE = "investment_price_points";
-  /// Legacy table removed in v10; kept for migration reference only.
-  static const INVESTMENT_CASH = "investment_cash";
   static const INVESTMENT_OTHER_ASSET = "investment_other_assets";
-  static const int DB_VERSION = 10;
+  static const int DB_VERSION = 1;
 }
